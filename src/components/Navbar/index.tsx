920c984b1e3891a3c4dfc446fcfb8d8253678cfc
Ez <126034682+CodingHTML-Yusef@users.noreply.github.com>
2023-05-19 09:33:59 -0400

    Update index.tsx

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -8,10 +8,10 @@ export default function Navbar(): JSX.Element {
     <nav>
       <div class="flex items-center justify-center md:justify-between w-full sm:px-16 md:px-20 lg:px-32 shadow-2xl">
         <div class="flex gap-5 py-5">
-          <a href="/" title="Radon Games">
+          <a href="/" title="Glatic Games">
             <img
-              src="/icons/transparent.png"
-              alt="Radon Games"
+              src="https://cdn.glitch.global/e15741ca-f3f8-4263-af96-21cfc1426961/Screenshot_2023-03-24_12.17.25_PM-removebg-preview.png?v=1679757457760"
+              alt="Glatic"
               class="h-6"
             ></img>
           </a>
@@ -22,7 +22,7 @@ export default function Navbar(): JSX.Element {
         <div class="hidden md:flex gap-5 items-center justify-center">
           <NavSearchbar />
           <IconButton
-            href="https://discord.gg/C2fbK35Rhg"
+            href="https://discord.gg/CwdfrG4Zyu"
             text="Discord"
             type="fa-brands"
             icon="fa-discord"
