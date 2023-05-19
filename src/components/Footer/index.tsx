7292527035bcadf882a66d0f6b3c5845f19385f0
Ez <126034682+CodingHTML-Yusef@users.noreply.github.com>
2023-05-19 08:54:18 -0400

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,7 +8,7 @@ export default function Navbar(): JSX.Element {
   return (
     <footer class="my-10 px-8 sm:px-16 md:px-20 lg:px-32 flex gap-10 flex-col md:flex-row justify-center items-center md:justify-between">
       <div class="flex flex-col gap-5">
-        <img src={banner} alt="Radon Games" class="h-6 w-min" />
+        <img src="" alt="Glatic class="h-6 w-min" />
         <p class="w-96 text-sm">
           An open-source unblocked games website built with simplicity in mind.
         </p>
@@ -45,20 +45,16 @@ export default function Navbar(): JSX.Element {
         <div class="flex flex-col gap-2">
           <FooterLink text="Partners" href="/partners" />
           <FooterLink text="Settings" href="/options" />
-          <FooterLink text="Privacy" href="/privacy" />
+          <FooterLink text="Privacy" href="https://quandaledingle.site/tos" />
         </div>
         <div class="flex flex-col gap-2">
           <FooterLink
             text="Request"
-            href="https://github.com/Radon-Games/Radon-Games/issues/new?assignees=&labels=game%20request&template=game_request.yaml"
+            href="https://docs.google.com/forms/d/e/1FAIpQLScYXyqfCQR4fCzlnpr-zW1rgEw72qf7-xjhR20ZZy0g9elTRA/viewform?embedded=true"
           />
           <FooterLink
             text="Report"
-            href="https://github.com/Radon-Games/Radon-Games/issues/new?assignees=&labels=bug&template=bug_report.yaml"
-          />
-          <FooterLink
-            text="Patreon"
-            href="https://www.patreon.com/Radon_Games"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSfSAYTIV4AtdhmNkA7YpGAZgaIzgOM3U4_Ie997L-ZskxiC5g/viewform?embedded=true"
           />
         </div>
       </div>
